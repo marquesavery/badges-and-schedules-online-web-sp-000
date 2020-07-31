5fa79b9bb8478971a4c76f92f402bfce1397b39c
Marques Avery <averymarques4@gmail.com>
2020-07-31 16:06:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+badge_list = []
+  array.each do |name|
+    badge_list << "Hello, my name is #{name}."
+  end
+  return badge_list
+end
+
+def assign_rooms
+
+end
